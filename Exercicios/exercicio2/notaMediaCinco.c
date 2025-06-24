af545c474ce0f768092e62228f0515cf23913400
Murilo Chagas <154563139+Silvaskrtt@users.noreply.github.com>
2025-06-24 15:57:32 -0300

    Finalizado o Exc7

--- Exercicios/exercicio2/notaMediaCinco.c
+++ Exercicios/exercicio2/notaMediaCinco.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <windows.h>
 
+    float media(float nota[5]){
+        float resultadomedia;
+        resultadomedia = (nota[0] + nota[1] + nota[2] + nota[3] + nota[4]) / 5.0;
+        return(resultadomedia);
+    }
+
 int main() {
+
+    // Declarando variaveis
+    float resultadomedia, nota[5];
+    int nota_valida = 0;
+
     // Mensagem de boas-vindas
     printf("--- Bem-Vindo ao seu calculador de Media ---\n");
     Sleep(3000);  // Espera 3 segundos
     system("cls");  // Limpa a tela
 
     // Verificando as notas se as notas sao validas
-    float nota[3];
-    int nota_valida = 0;
-
     while (nota_valida == 0) {
         nota_valida = 1; 
         printf("Por favor, digite as suas 5 notas (nao insira valores negativos)\n");
@@ -26,10 +34,19 @@ int main() {
             }
         }
     }
+    //limpa tela
+    system("cls");
 
-    // Calculando e exibindo a média
-    float media = (nota[0] + nota[1] + nota[2] + nota[3] + nota[4]) / 5.0;
-    printf("Sua media e: %.2f\n", media);
+    // chamando a funçao media
+    resultadomedia = media(nota);
+
+    if(resultadomedia >= 7.0){
+        printf("Sua media e: %.2f\nVoce esta aprovado!", resultadomedia);
+    }else if(resultadomedia >= 6.0){
+        printf("Sua media e: %.2f\nVoce esta de recuperacao!", resultadomedia);
+    }else{
+        printf("Ta reprovado vagabundo!\n%.2f tua media ai!", resultadomedia);
+    }
 
     return 0;
 }
