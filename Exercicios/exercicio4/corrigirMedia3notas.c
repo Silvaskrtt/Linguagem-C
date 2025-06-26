21ff9b983da35b14af584c5004a2ada5225d0925
Silvaskrtt <murilodasilvadaschagas12@gmail.com>
2025-06-26 19:01:59 -0300

    Corrigir

--- Exercicios/exercicio4/corrigirMedia3notas.c
+++ Exercicios/exercicio4/corrigirMedia3notas.c
@@ -4,18 +4,24 @@
 
     float media(float vetmed[3]){
         float resultMedia;
-        resultMedia = (vetmed[0]+vetmed[1]+vetmed[2]/3);
+        resultMedia = (vetmed[0]+vetmed[1]+vetmed[2])/3;
         return(resultMedia);
     }
 
     int main(){
 
         //Declarando variaveis
-        float resultMedia;
+        float resultMedia, vetmed[3];
+        int pos;
 
         //Pedindo as notas para o aluno
+        for(pos = 0; pos < 3; pos++){
+            printf("Digite a %d nota: ", pos+1);
+            scanf(" %f", &vetmed[pos]);
+        }
+
+        resultMedia = media(vetmed);
+        printf("Sua media anual = %.2f", resultMedia);
         
-        
-        printf("Media: %.1f\n", media);
-        return;
+         return 0;
     }
