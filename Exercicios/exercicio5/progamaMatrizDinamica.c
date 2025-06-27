620967ef4b98bddf7078d56ba2737e2ac557b69f
Silvaskrtt <murilodasilvadaschagas12@gmail.com>
2025-06-26 21:30:18 -0300

    atemeianoite

---
+++ Exercicios/exercicio5/progamaMatrizDinamica.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+    int main(){
+        // Declarando
+        int m, n;
+
+        // Mensagem de boas vindas
+        printf("Bem Vindo ao Sua matriz dinamica!");
+        Sleep(3000);
+        system("cls");
+
+        printf("Digite o primeiro valor: ");
+        scanf(" %d", )
+
+    }
